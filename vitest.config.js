3e5261701c05bddfc9a99685ffb065c197cfe022
Sam Carlton <sam@sam.lc>
2023-02-04 19:45:13 -0600

    Configure vite-tsconfig-paths for vitest

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // import astroConfig from './astro.config.mjs'
 
 // https://vitejs.dev/config/
 export default defineConfig( {
     // ...astroConfig.vite,
+    plugins: [
+        tsconfigPaths(),
+    ],
 } )
