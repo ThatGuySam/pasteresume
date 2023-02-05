4c98fd835365cfff15b8f7d3e3bc94f3c24ae275
Sam Carlton <sam@sam.lc>
2023-02-05 16:34:38 -0600

    Add Query form

--- src/components/queries.vue
+++ src/components/queries.vue
@@ -7,7 +7,89 @@
         >
             <h1>Queries</h1>
             <div>
-                <ul role="list" class="divide-y divide-gray-200">
+                <div v-if="!hasAnyStoredQueries || showForm">
+                    <form
+                        class="isolate -space-y-px rounded-md shadow-sm"
+                        @submit.prevent="showForm = false"
+                    >
+                        <div class="relative rounded-md rounded-b-none border border-gray-300 px-3 py-2 focus-within:z-10 focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+                            <label for="name" class="block text-xs font-medium opacity-75">Terms</label>
+                            <input
+                                id="terms"
+                                v-model="options.terms"
+                                type="text"
+                                name="terms"
+                                class="block w-full border-0 p-0 bg-transparent placeholder-gray-500 focus:ring-0 sm:text-sm"
+                                placeholder="Remote Marketing Director"
+                                autofocus
+                            >
+                        </div>
+                        <div class="flex -space-x-px">
+                            <div class="w-1/2 min-w-0 flex-1 border border-gray-300 focus-within:z-10 focus-within:border-indigo-500 focus-within:ring-indigo-500 px-3 py-2">
+                                <label for="min" class="block text-xs font-medium opacity-75">Min</label>
+                                <input
+                                    id="min"
+                                    v-model="options.salary.min"
+                                    type="number"
+                                    :min="50_000"
+                                    :step="10_000"
+                                    name="min"
+                                    class="relative block w-full bg-transparent border-0 focus:ring-0 sm:text-sm p-0"
+                                >
+                            </div>
+                            <div class="min-w-0 flex-1 border border-gray-300 focus:z-10 focus:border-indigo-500 focus:ring-indigo-500 px-3 py-2">
+                                <label for="max" class="block text-xs font-medium opacity-75">Max</label>
+                                <input
+                                    id="max"
+                                    v-model="options.salary.max"
+                                    type="number"
+                                    :max="900_000"
+                                    :step="10_000"
+                                    name="max"
+                                    class="relative block w-full bg-transparent border-0 focus:ring-0 sm:text-sm p-0"
+                                >
+                            </div>
+                        </div>
+                        <div class="relative border border-gray-300 px-3 py-2 focus-within:z-10 focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+                            <label class="block text-xs font-medium opacity-75">Perks</label>
+                            <div class="flex flex-wrap mt-4 gap-4">
+                                <div
+                                    v-for="(perk, perkIndex) in options.perks"
+                                    :key="perk"
+                                    class="relative flex items-start gap-1"
+                                >
+                                    <div class="flex h-5 items-center">
+                                        <input
+                                            :id="`perk-${perkIndex}`"
+                                            name="comments"
+                                            type="checkbox"
+                                            class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                            :checked="perkInTerms(perk)"
+                                            @change="togglePerk(perk)"
+                                        >
+                                    </div>
+                                    <div class="text-sm">
+                                        <label :for="`perk-${perkIndex}`" class="font-medium text-xs">{{ perk }}</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button
+                            type="submit"
+                            :disable="!hasTerms"
+                            class="inline-flex w-full justify-center rounded-md rounded-t-none border border-transparent bg-indigo-600 py-2 px-4 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            :class="[
+                                !hasTerms && 'opacity-50 cursor-not-allowed',
+                            ]"
+                        >
+                            Show
+                        </button>
+                    </form>
+                </div>
+                <ul
+                    v-else
+                    role="list" class="divide-y divide-gray-200"
+                >
                     <li v-for="activityItem in activityItems" :key="activityItem.id" class="py-4">
                         <div class="flex space-x-3">
                             <img class="h-6 w-6 rounded-full" :src="activityItem.person.imageUrl" alt="">
@@ -55,9 +137,43 @@ export default {
     data () {
         return {
             loaded: false,
+            showForm: false,
+            targetQuery: '',
+            options: {
+                terms: '',
+                salary: {
+                    min: 70_000,
+                    max: 250_000,
+                },
+                perks: [
+                    'Remote',
+                    'Health Insurance',
+                    '401k',
+                    'Paid Time Off',
+                    'Stock Options',
+                    'Flexible Hours',
+                    // 'Insurance',
+                    // 'Dental Insurance',
+                    // 'Vision Insurance',
+                    // 'Life Insurance',
+                    // 'Parental Leave',
+                    // 'Maternity Leave',
+                    // 'Paternity Leave',
+                    // 'Disability Insurance',
+                    // 'Gym Membership',
+                ],
+            },
             people,
             activityItems,
         }
+    },
+    computed: {
+        hasAnyStoredQueries () {
+            return false
+        },
+        hasTerms () {
+            return this.options.terms.trim().length > 0
+        },
     },
     mounted () {
         setTimeout( () => {
@@ -65,7 +181,17 @@ export default {
         }, 2000 )
     },
     methods: {
+        perkInTerms ( perk ) {
+            return this.options.terms.toLowerCase().includes( perk.toLowerCase() )
+        },
+        togglePerk ( perk ) {
+            if ( this.perkInTerms( perk ) ) {
+                this.options.terms = this.options.terms.replace( perk, '' ).trim()
+                return
+            }
 
+            this.options.terms += ` ${ perk }`
+        },
     },
 }
 </script>
