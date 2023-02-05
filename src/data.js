470dc230ae4d87c181c47d9317e9ecef3029b69b
Sam Carlton <sam@sam.lc>
2023-02-04 19:18:36 -0600

    Disable header links

--- src/data.js
+++ src/data.js
@@ -1,65 +1,65 @@
-import { getAsset, getBlogPermalink, getPermalink } from './utils/permalinks'
+import { getAsset, getPermalink } from './utils/permalinks'
 
 export const headerData = {
     links: [
-        {
-            text: 'Landing',
-            links: [
-                {
-                    text: 'Sass',
-                    href: getPermalink( '/landing/saas' ),
-                },
-                {
-                    text: 'Startup',
-                    href: getPermalink( '/landing/startup' ),
-                },
-                {
-                    text: 'Mobile App',
-                    href: getPermalink( '/landing/mobile-app' ),
-                },
-            ],
-        },
-        {
-            text: 'Pages',
-            links: [
-                {
-                    text: 'Features',
-                    href: '#',
-                },
-                {
-                    text: 'Pricing',
-                    href: '#',
-                },
-                {
-                    text: 'About us',
-                    href: '#',
-                },
-                {
-                    text: 'Contact',
-                    href: '#',
-                },
-                {
-                    text: 'Terms',
-                    href: getPermalink( '/terms' ),
-                },
-                {
-                    text: 'Privacy policy',
-                    href: getPermalink( '/privacy' ),
-                },
-            ],
-        },
-        {
-            text: 'Widgets',
-            href: '#',
-        },
-        {
-            text: 'Blog',
-            href: getBlogPermalink(),
-        },
+        // {
+        //     text: 'Landing',
+        //     links: [
+        //         {
+        //             text: 'Sass',
+        //             href: getPermalink( '/landing/saas' ),
+        //         },
+        //         {
+        //             text: 'Startup',
+        //             href: getPermalink( '/landing/startup' ),
+        //         },
+        //         {
+        //             text: 'Mobile App',
+        //             href: getPermalink( '/landing/mobile-app' ),
+        //         },
+        //     ],
+        // },
+        // {
+        //     text: 'Pages',
+        //     links: [
+        //         {
+        //             text: 'Features',
+        //             href: '#',
+        //         },
+        //         {
+        //             text: 'Pricing',
+        //             href: '#',
+        //         },
+        //         {
+        //             text: 'About us',
+        //             href: '#',
+        //         },
+        //         {
+        //             text: 'Contact',
+        //             href: '#',
+        //         },
+        //         {
+        //             text: 'Terms',
+        //             href: getPermalink( '/terms' ),
+        //         },
+        //         {
+        //             text: 'Privacy policy',
+        //             href: getPermalink( '/privacy' ),
+        //         },
+        //     ],
+        // },
+        // {
+        //     text: 'Widgets',
+        //     href: '#',
+        // },
+        // {
+        //     text: 'Blog',
+        //     href: getBlogPermalink(),
+        // },
     ],
-    actions: [
-        { type: 'button', text: 'Download', href: 'https://github.com/onwidget/astrowind' },
-    ],
+    // actions: [
+    //     { type: 'button', text: 'Download', href: 'https://github.com/onwidget/astrowind' },
+    // ],
 }
 
 export const footerData = {
