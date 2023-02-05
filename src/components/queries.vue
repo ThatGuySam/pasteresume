9604faea010a140e4a72dd06296d6296695eba2b
Sam Carlton <sam@sam.lc>
2023-02-05 14:04:39 -0600

    Limit layout to phone width

--- src/components/queries.vue
+++ src/components/queries.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>
-        <h1>Queries</h1>
+    <div class="wrapper flex justify-center">
+        <div class="container max-w-sm border">
+            <h1>Queries</h1>
+        </div>
     </div>
 </template>
