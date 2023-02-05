ea10374f5e967866e9caf8640024107ff522aac5
Sam Carlton <sam@sam.lc>
2023-02-04 19:11:11 -0600

    Lint config file

--- src/config.mjs
+++ src/config.mjs
@@ -1,62 +1,62 @@
-import defaultImage from './assets/images/default.png';
+import defaultImage from './assets/images/default.png'
 
 const CONFIG = {
-  name: 'Paste Resume',
+    name: 'Paste Resume',
 
-  origin: 'https://pasteresume.com',
-  basePathname: '/',
-  trailingSlash: false,
+    origin: 'https://pasteresume.com',
+    basePathname: '/',
+    trailingSlash: false,
 
-  title: 'Paste Resume — Free template for create a website with Astro + Tailwind CSS',
-  description:
+    title: 'Paste Resume — Free template for create a website with Astro + Tailwind CSS',
+    description:
     '🚀 Suitable for Startups, Small Business, Sass Websites, Professional Portfolios, Marketing Websites, Landing Pages & Blogs.',
-  defaultImage: defaultImage,
+    defaultImage,
 
-  defaultTheme: 'system', // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
+    defaultTheme: 'system', // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
 
-  language: 'en',
-  textDirection: 'ltr',
+    language: 'en',
+    textDirection: 'ltr',
 
-  dateFormatter: new Intl.DateTimeFormat('en', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-    timeZone: 'UTC',
-  }),
+    dateFormatter: new Intl.DateTimeFormat( 'en', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        timeZone: 'UTC',
+    } ),
 
-  googleAnalyticsId: false, // or "G-XXXXXXXXXX",
-  googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
+    googleAnalyticsId: false, // or "G-XXXXXXXXXX",
+    googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
 
-  blog: {
-    disabled: false,
-    postsPerPage: 4,
+    blog: {
+        disabled: false,
+        postsPerPage: 4,
 
-    post: {
-      permalink: '/%slug%', // Variables: %slug%, %year%, %month%, %day%, %hour%, %minute%, %second%, %category%
-      noindex: false,
-      disabled: false,
+        post: {
+            permalink: '/%slug%', // Variables: %slug%, %year%, %month%, %day%, %hour%, %minute%, %second%, %category%
+            noindex: false,
+            disabled: false,
+        },
+
+        list: {
+            pathname: 'blog', // Blog main path, you can change this to "articles" (/articles)
+            noindex: false,
+            disabled: false,
+        },
+
+        category: {
+            pathname: 'category', // Category main path /category/some-category
+            noindex: true,
+            disabled: false,
+        },
+
+        tag: {
+            pathname: 'tag', // Tag main path /tag/some-tag
+            noindex: true,
+            disabled: false,
+        },
     },
+}
 
-    list: {
-      pathname: 'blog', // Blog main path, you can change this to "articles" (/articles)
-      noindex: false,
-      disabled: false,
-    },
-
-    category: {
-      pathname: 'category', // Category main path /category/some-category
-      noindex: true,
-      disabled: false,
-    },
-
-    tag: {
-      pathname: 'tag', // Tag main path /tag/some-tag
-      noindex: true,
-      disabled: false,
-    },
-  },
-};
-
-export const SITE = { ...CONFIG, blog: undefined };
-export const BLOG = CONFIG.blog;
-export const DATE_FORMATTER = CONFIG.dateFormatter;
+export const SITE = { ...CONFIG, blog: undefined }
+export const BLOG = CONFIG.blog
+export const DATE_FORMATTER = CONFIG.dateFormatter
