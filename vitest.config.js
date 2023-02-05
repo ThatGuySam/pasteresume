b43556ed37b44f8920b87373f3fe07ae8b0eef88
Sam Carlton <sam@sam.lc>
2023-02-04 19:27:37 -0600

    Configure vitest

---
+++ vitest.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vitest/config'
+
+// import astroConfig from './astro.config.mjs'
+
+// https://vitejs.dev/config/
+export default defineConfig( {
+    // ...astroConfig.vite,
+} )
