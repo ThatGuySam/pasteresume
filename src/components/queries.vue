646fcec60aa43a801a2f4d9e2f34441836834c7a
Sam Carlton <sam@sam.lc>
2023-02-06 21:11:34 -0600

    Add New Terms to bottom of list

--- src/components/queries.vue
+++ src/components/queries.vue
@@ -134,6 +134,13 @@
                             </a>
                         </li>
                     </ul>
+                    <button
+                        type="button"
+                        class="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        @click="showForm = true"
+                    >
+                        New Terms
+                    </button>
                 </div>
             </div>
         </div>
